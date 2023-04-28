49e308d9e5f30ad0f00b0a3213542a02ae501289
Humberto Juarez <humberto.juarez@gmail.com>
2023-04-27 21:50:04 -0300

    borrando archivos de carpeta utils en rama master

--- utils/apolloClient.js
+++
@@ -1,8 +0,0 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client'
-
-const createApolloClient = () => new ApolloClient({
-  uri: 'http://192.168.100.154:4000',
-  cache: new InMemoryCache()
-})
-
-export default createApolloClient
